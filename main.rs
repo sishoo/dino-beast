8ff8fe919adaea57d49b1f43e8a185a055231c1b
sishoo <112181983+sishoo@users.noreply.github.com>
2023-09-20 20:27:55 -0400

    Update main.rs

--- main.rs
+++ main.rs
@@ -4,41 +4,54 @@ use ggez::{Context, GameResult};
 use ggez::glam::*;
 use rand::prelude::*;
 
-const NUM_DINOS: usize = 100;
-const NUM_FOOD: usize = 100;
+const NUM_DINOS: usize = 1;
+const NUM_FOOD: usize = 1;
 const SCREEN_WIDTH: f32 = 1000.0;
 const SCREEN_HEIGHT: f32 = 800.0;
+const STEP: f32 = 0.5;
 
 struct MainState {
-    dino_x: [f32; NUM_DINOS],
-    dino_y: [f32; NUM_DINOS],
-    dino_dist: [f32; NUM_DINOS],
-    food_x: [f32; NUM_FOOD],
-    food_y: [f32; NUM_FOOD]
+    dino_x: Vec<f32>,
+    dino_y: Vec<f32>,
+    dino_dist: Vec<f32>,
+    food_x: Vec<f32>,
+    food_y: Vec<f32>,
+    closest_food: Vec<usize>
 }
 
 impl MainState {
     fn new() -> GameResult<Self> {
         let mut rng = rand::thread_rng();
-        let mut dino_x = [0.0; NUM_DINOS];
-        let mut dino_y = [0.0; NUM_DINOS];
-        let mut food_x = [0.0; NUM_FOOD];
-        let mut food_y = [0.0; NUM_FOOD];
-        let mut dino_dist = [0.0; NUM_DINOS];
+        let mut dino_x: Vec<f32> = Vec::with_capacity(NUM_DINOS);
+        let mut dino_y: Vec<f32> = Vec::with_capacity(NUM_DINOS);
+        let mut food_x: Vec<f32> = Vec::with_capacity(NUM_FOOD);
+        let mut food_y: Vec<f32> = Vec::with_capacity(NUM_FOOD);
+        let mut dino_dist: Vec<f32> = Vec::with_capacity(NUM_DINOS);
+        let mut closest_food: Vec<usize> = Vec::with_capacity(NUM_DINOS);
 
-        dino_x.iter_mut().for_each(|x| *x = rng.gen_range(0.0..SCREEN_WIDTH));
-        dino_y.iter_mut().for_each(|y| *y = rng.gen_range(0.0..SCREEN_HEIGHT));
-        food_x.iter_mut().for_each(|x| *x = rng.gen_range(0.0..SCREEN_WIDTH));
-        food_y.iter_mut().for_each(|y| *y = rng.gen_range(0.0..SCREEN_HEIGHT));
+        for _ in 0..NUM_DINOS {
+            dino_x.push(rng.gen_range(0.0..SCREEN_WIDTH));
+            dino_y.push(rng.gen_range(0.0..SCREEN_HEIGHT));
+        }
+        for _ in 0..NUM_FOOD {
+            food_x.push(rng.gen_range(0.0..SCREEN_WIDTH));
+            food_y.push(rng.gen_range(0.0..SCREEN_HEIGHT));
+        }
 
         for dino in 0..NUM_DINOS {
             let dino_x = dino_x[dino];
             let dino_y = dino_y[dino];
-            let mut distances = Vec::with_capacity(NUM_DINOS);
+            let mut min_dist = f32::MAX;
+            let mut mindex: usize = 0;
             for food in 0..NUM_FOOD {
-                distances.push(euclidian_dist_between(dino_x, dino_y, food_x[food], food_y[food]));
+                let dist = dist_of(dino_x, dino_y, food_x[food], food_y[food]);
+                if dist < min_dist {
+                    min_dist = dist;
+                    mindex = food;
+                }
             }
-            dino_dist[dino] = distances.iter().min();
+            dino_dist.push(min_dist);
+            closest_food.push(mindex);
         }
 
         Ok(Self {
@@ -46,14 +59,28 @@ impl MainState {
             dino_y,
             dino_dist,
             food_x,
-            food_y
+            food_y,
+            closest_food
         })
     }
 }
 
 impl event::EventHandler<ggez::GameError> for MainState {
-    fn update(&mut self, _ctx: &mut Context) -> GameResult {
-
+    fn update(&mut self, ctx: &mut Context) -> GameResult {
+        // think back to circle cut in half thing
+        for dino in 0..NUM_DINOS {
+            let mut dino_x = self.dino_x[dino];
+            let mut dino_y = self.dino_y[dino];
+            let mut current_dist = self.dino_dist[dino];
+            let food_index = self.closest_food[dino];
+            let food_x = self.food_x[food_index];
+            let food_y = self.food_y[food_index];
+            
+            self.dino_x[dino] = dino_x;
+            self.dino_y[dino] = dino_y;
+            self.dino_dist[dino] = current_dist;
+        }
+        println!("fps {:?}", ctx.time.fps());
         Ok(())
     }
 
@@ -92,13 +119,13 @@ impl event::EventHandler<ggez::GameError> for MainState {
     }
 }
 
-fn euclidian_dist_between(x1: f32, x2: f32, y1: f32, y2: f32) -> f32 {
+fn dist_of(x1: f32, x2: f32, y1: f32, y2: f32) -> f32 {
     ((x2 - x1) * (x2 - x1) + (y2 - y1) * (y2 - y1)).sqrt()
 }
 
 fn main() -> GameResult {
-    assert!(NUM_DINOS == 0);
-    assert!(NUM_FOOD == 0);
+    assert!(NUM_DINOS != 0);
+    assert!(NUM_FOOD != 0);
     let cb = ggez::ContextBuilder::new("Dino Beast", "Mac")
         .window_setup(ggez::conf::WindowSetup::default().title("Dino Beast"))
         .window_mode(ggez::conf::WindowMode::default().dimensions(SCREEN_WIDTH, SCREEN_HEIGHT));
